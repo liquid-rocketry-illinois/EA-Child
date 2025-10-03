b6d4fa6ad8215629ea7877469387901a3655a78a
JasonZhang <jasonfz2025@gmail.com>
2025-10-02 20:52:50 -0500

    Sensors sudo code

--- src/Sensors/Sensors.cpp
+++ src/Sensors/Sensors.cpp
@@ -2,12 +2,19 @@
 
 bool Sensors::InitSensors(){
     Sensors::IMUs.Init();
+    //Sensors::MS.Init();
+    //Sensors::Temp.Init();
     // Add init functions for other sensors
-
+    
     return (IMUs.MainStatus && IMUs.SecStatus); // Add statuses for the other sensors
+    //Add MS and Temp statuses
 }
 
 // Separating Sensors because idfk actually
 void Sensors::Update(){
     Sensors::IMUs.Update();
+    Sensors::MS.Update();
+    //Sensors::Temp.Update();
+
+    
 }
--- src/Sensors/Sensors.h
+++ src/Sensors/Sensors.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "IMU_Sensors.h"
-
+//#include "MS_Sensor.h"
+//#include "SHIT 40.h"
 struct Sensors{
     IMUSensors IMUs;
-
+    //MSSensors MS;
+    //Temperature Sensors Temp;
+    
     bool InitSensors();
     void Update();
 };
